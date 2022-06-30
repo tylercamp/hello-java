9bfff21c7ebdfcbfcc558c0921c943de48d5ab50
Ron Felice <40459311+rofelice@users.noreply.github.com>
2022-06-29 20:12:20 -0400

    Update VerifyPass.java

--- src/main/java/VerifyPass.java
+++ src/main/java/VerifyPass.java
@@ -1,8 +1,10 @@
-public boolean VerifyAdmin(String password) {
-  if (password.equals("68af404b513073584c4b6f22b6c63e6b")) {
-    System.out.println("Entering Diagnostic Mode...");
-    return true;
+public class HardCodedCryptoExample{
+  public boolean VerifyAdmin(String password) {
+    if (password.equals("68af404b513073584c4b6f22b6c63e6b")) {
+      System.out.println("Entering Diagnostic Mode...");
+      return true;
+    }
+    System.out.println("Incorrect Password!");
+    return false;
   }
-  System.out.println("Incorrect Password!");
-  return false;
 }
