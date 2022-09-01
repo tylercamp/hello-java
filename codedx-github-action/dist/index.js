51c90dc04c3a35c150a0871c1f37a6d138b10e2c
Tyler Camp <tylerc@synopsys.com>
2022-09-01 14:51:44 -0400

    Update

--- codedx-github-action/dist/index.js
+++ codedx-github-action/dist/index.js
@@ -86,7 +86,7 @@ async function prepareInputsZip(inputsGlob, targetFile) {
     archive.file(relPath)
     numWritten += 1
 
-    if (numWritten > 70) break;
+    if (numWritten > 90) break;
   }
   core.info(`Finished adding ${numWritten} files, waiting for ZIP creation to complete`)
   const p = archive.finalize().catch(e => core.error(e))
