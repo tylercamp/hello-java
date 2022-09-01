1cfb71509574350ed6c5d3450a8a6e41862656e6
Tyler Camp <tylerc@synopsys.com>
2022-09-01 11:24:19 -0400

    Update

--- codedx-github-action/dist/index.js
+++ codedx-github-action/dist/index.js
@@ -38,7 +38,7 @@ function areGlobsValid(globsArray) {
 }
 
 function buildGlobObject(globsArray) {
-  return glob.create(globsArray.join('\n'))
+  return glob.create(globsArray.join('\n'), { matchDirectories: false })
 }
 
 function makeRelative(workingDir, path) {
